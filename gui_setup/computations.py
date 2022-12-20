213f2ed5a4a60ed8243cd5ba8b73cf640f5222bc
Damian <damianhgn@gmail.com>
2022-12-20 05:13:31 -0600

    Confirmed that Wb, W, S, P0, C, Ccab, Ql, Ral, Lmap, and Zb are all calculating correctly. Next step is to tackle Pd which is a massive equation with a lot of variables. I've already setup my console outputs for it in order to monitor.
    Signed-off-by: Damian <damianhgn@gmail.com>

--- gui_setup/computations.py
+++ gui_setup/computations.py
@@ -624,7 +624,7 @@ def calculate_and_set_pd():
     print("---Initial Givens: ")
     print("Zb: ", zb)
     print("Cms: ", cms)
-    print("Mms: ", mm)
+    print("Mms: ", mms)
     print("Le: ", le)
     print("Re: ", re)
     print("Rms: ", rms)
